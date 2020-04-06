4a1427f11e8344b702c8173bfe8d2f18e201f1df
Vladimir Tanski <38607979+TanskiV@users.noreply.github.com>
2020-04-06 18:42:15 +0300

    Add files via upload

---
+++ precache-manifest.67f1451a1cba4d0e895707f1e6ddc0d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "762fd01099d4a8ed1ac2da5741e37e19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2d980e643117366eb25",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "b339646ebf95dc40f95b",
+    "url": "/static/css/main.6d57da9b.chunk.css"
+  },
+  {
+    "revision": "c2d980e643117366eb25",
+    "url": "/static/js/2.b4ccea76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b4ccea76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b339646ebf95dc40f95b",
+    "url": "/static/js/main.e5f26df7.chunk.js"
+  },
+  {
+    "revision": "ff561b555801cb57d25f",
+    "url": "/static/js/runtime-main.ea36d035.js"
+  }
+]);
